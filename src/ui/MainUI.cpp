571257637faebf87325055e0151b87586abc730e
suarkadipa <suarkadipa@gmail.com>
2016-06-20 09:28:50 +0800

    updated, remove deprecated on open file chooser dialog

--- src/ui/MainUI.cpp
+++ src/ui/MainUI.cpp
@@ -174,8 +174,8 @@ namespace Mitrais
 			dialog = gtk_file_chooser_dialog_new ("Chose file..",
 			     GTK_WINDOW(window),
 			     GTK_FILE_CHOOSER_ACTION_OPEN,
-			     GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-			     GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+				 ("_Cancel"), GTK_RESPONSE_CANCEL,
+				 ("_Open"), GTK_RESPONSE_ACCEPT,
 			     NULL);
 
 		   if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT)
