1221a0f72fe989e6cb62a524c6c05a456201192c
Thomas Rothe <eingefuchst@gmail.com>
2016-06-23 11:28:19 +0800

    changed style of tests

---
+++ test/main.cpp
@@ -0,0 +1,21 @@
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+#include "TestSocketConnection.h"
+#include "../inc/SocketConnection.h"
+
+using namespace Mitrais::util;
+
+int main()
+{
+	CppUnit::TextUi::TestRunner runner;
+	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+
+	runner.addTest( registry.makeTest() );
+	bool wasSuccessful = runner.run();
+
+	return 0;
+}
+
+
