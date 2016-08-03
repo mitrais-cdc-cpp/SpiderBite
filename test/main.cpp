eb6ce86819268d2bde3703bdbafbb5d446723346
azisfatoni <azisfatoni@gmail.com>
2016-08-03 15:03:17 +0700

    Update main unit test for prevent running twice the unit test.

--- test/main.cpp
+++ test/main.cpp
@@ -2,12 +2,7 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
-#include "TestSocketConnection.h"
 #include "../inc/SocketConnection.h"
-#include "TestTextReader.h"
-#include "../inc/TextReader.h"
-#include "TestTextBuffer.h"
-#include "../inc/TextBuffer.h"
 
 using namespace Mitrais::util;
 
@@ -21,5 +16,3 @@ int main()
 
 	return 0;
 }
-
-
